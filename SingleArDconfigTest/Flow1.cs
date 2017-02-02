ef6307df0a64295bffd419bc9c712137d68b9d0b
Georg Kapeller <gkapeller@ranorex.com.local>
2017-02-02 13:07:44 +0100

    first working example

---
+++ SingleArDconfigTest/Flow1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by Ranorex
+ * User: gkapeller
+ * Date: 2/2/2017
+ * Time: 12:36 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+using Ranorex.Core.Reporting;
+
+namespace SingleArDconfigTest
+{
+	/// <summary>
+	/// Description of Flow1.
+	/// </summary>
+	public class Flow1
+	{
+		public Flow1()
+		{
+		}
+		
+		public static void run()
+		{
+			//TestReport.BeginTestSuite("MyTestSuite");
+			
+			Flow1_Path1.Start();
+			Flow1_Path2.Start();
+		}
+	}
+}
